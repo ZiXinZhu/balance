777d529c97ca970b5a065a253d912492bed4a096
husky <1101648204@qq.com>
2019-04-22 11:31:23 +0800

    MVC让html路径隐藏

--- src/main/java/com/zzx/balance/cfg/MVC.java
+++ src/main/java/com/zzx/balance/cfg/MVC.java
@@ -1,21 +1,22 @@
 package com.zzx.balance.cfg;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class MVCConfig implements WebMvcConfigurer{
 
-    @Override
+@Component
+public class MVC implements WebMvcConfigurer{
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/","/index");
-        registry.addViewController("/index").setViewName("404");
+        registry.addRedirectViewController("/","/404");
+        registry.addViewController("/zzx").setViewName("test");
+        registry.addViewController("/404").setViewName("404");
     }
+}
 
 //    @Override
 //    public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(null);
 //    }
-}
+//}
